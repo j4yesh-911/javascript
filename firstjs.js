676aae9fde249b19e812c1f65dadbc6579e98943
j4yesh-911 <jayeshgayri12@gmail.com>
2025-07-23 22:28:27 +0530

    js

--- firstjs.js
+++ firstjs.js
@@ -1,1 +1,12 @@
 //hello
+const student ={
+fullName : "jayesh gayri",
+year: "3rd",
+branch: "CSE",
+rollNumber : 57,
+}
+// console.log(student);
+student ["rollNumber"]= student["rollNumber"]+1;
+student ["year"]= "III year";
+console.log(student["rollNumber"]);
+console.log(student);
