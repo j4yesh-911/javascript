beb4427c6a0ff95fbed9ad3e9e706b0e5fba7e79
j4yesh-911 <jayeshgayri12@gmail.com>
2025-07-24 22:09:14 +0530

    tablejs

--- firstjs.js
+++ firstjs.js
@@ -1,12 +1,21 @@
 //hello
-const student ={
-fullName : "jayesh gayri",
-year: "3rd",
-branch: "CSE",
-rollNumber : 57,
-}
+// const student ={
+// fullName : "jayesh gayri",
+// year: "3rd",
+// branch: "CSE",
+// rollNumber : 57,
+// }
+// // console.log(student);
+// student ["rollNumber"]= student["rollNumber"]+1;
+// student ["year"]= "III year";
+// console.log(student["rollNumber"]);
 // console.log(student);
-student ["rollNumber"]= student["rollNumber"]+1;
-student ["year"]= "III year";
-console.log(student["rollNumber"]);
-console.log(student);
+
+const accountId = 525;
+let accountName ='jayesh';
+var acckhyu = 'bank';
+accountState = "punjab";
+let kyure;
+
+console.log(accountId);
+console.table([acckhyu,accountName,accountState, kyure]);
