6578bc8c2df2183b668c9b404fe85dba7abbcd50
j4yesh-911 <jayeshgayri12@gmail.com>
2025-07-25 22:27:58 +0530

    js

--- firstjs.js
+++ firstjs.js
@@ -11,11 +11,65 @@
 // console.log(student["rollNumber"]);
 // console.log(student);
 
-const accountId = 525;
-let accountName ='jayesh';
-var acckhyu = 'bank';
-accountState = "punjab";
-let kyure;
 
-console.log(accountId);
-console.table([acckhyu,accountName,accountState, kyure]);
+
+//                  TBALE CREALTE ON CONSOLE 
+// const accountId = 525;
+// let accountName ='jayesh';
+// var acckhyu = 'bank';
+// accountState = "punjab";
+// let kyure;
+
+// console.log(accountId);
+// console.table([acckhyu,accountName,accountState, kyure]);
+
+
+
+
+
+//                   below this is for using new type of js 
+
+
+
+
+// "use script" // this !!
+
+//  //                    understanding the concept of null and undefined 
+// let age = "20"+3;
+// let state ;
+// let marks = null;
+
+
+// console.log(typeof age);
+// console.log(typeof state);
+// console.log(typeof marks);
+
+
+
+//                   CONVERSION
+
+
+
+
+// let mereNumber = "33+5abc"
+// console.log(typeof mereNumber);
+
+// let NNumber = Number(mereNumber)
+// console.log(typeof NNumber);
+// console.log(NNumber)
+
+// let number =""
+// let vari = Boolean(number);
+// console.log(vari)
+
+
+// console.log("1"+2+3+4+5)
+// console.log(1+2+"2"+2+3)
+// console.log(1+2+("b")+(2+3))
+// console.log(+true)
+// console.log(+"")
+
+// console.log("5"<2)
+
+// "==" doesnt check the data type ;
+// "===" check the datatype always of both values !!
