4edbd13bd12967adbb218381f46edd703af465cb
j4yesh-911 <jayeshgayri12@gmail.com>
2025-08-03 19:34:43 +0530

    arr_2 js

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,6 @@
-console.log(Array.of('jayesh'))
+
+let score_1 = 2005;
+let score_2 = 9115;
+let score_3 = 2265;
+console.log(Array.of(score_1,score_2,score_3))
+console.table(Array.of(score_1,score_2,score_3))
