42218b3503dbce3b50ebf9f3b5b90e885b519e57
j4yesh-911 <jayeshgayri12@gmail.com>
2025-08-20 23:06:33 +0530

    controlscopejs

---
+++ controlflow.js
@@ -0,0 +1,7 @@
+let mysalary =500000
+if (mysalary<500001){
+    console.log('paisa hi paisa hoga ')
+}
+
+if (mysalary===500000) console.log('fix h bhai sb'),
+console.log('dekh kya raha h naya synetx h code me dekh');
