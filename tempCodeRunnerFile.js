de8df53c932d82353299b3d037e4fd0ede4c5493
j4yesh-911 <jayeshgayri12@gmail.com>
2025-08-24 23:20:24 +0530

    jscontrol

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,11 +1,1 @@
-const sqr = squaree=()=> {
-    (number={
-        username:'jayesh',
-        future : square=(num1)=>{
-        console.log(this.username)
-        return num1**2
-    }
-    })
-    console.log(square(5))
-}
-squaree()
+ val= null ?? 10
