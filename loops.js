852c2e1a7af74b045b1e2da0c77859f49bbae1ed
j4yesh-911 <jayeshgayri12@gmail.com>
2025-08-27 22:07:43 +0530

    js loops

--- loops.js
+++ loops.js
@@ -1,3 +1,22 @@
-for (i=0; i<=1;i++){
-    console.log(i)
+// for (i=1; i<=10;i++){
+//      console.log("\n")
+//     console.log(`table of ${i}`)
+//     console.log("\n")
+//  for (j=1; j<=10;j++){
+//     console.log(`${i}*${j}=${i*j}`)
+//  }
+// }
+
+const arr=['jayesh', 'hari' , 'pankaj', 'yakshit', 'ashwin']
+for(i=0; i<arr.length;i++){
+    if (arr[i]== 'pankaj'){ 
+        console.log('pankaj detected\n');
+        // break
+    
+    continue
+   
+    }
+    console.log(arr[i]);
+    
+    
 }
